13213cfcb7ed6c96ff1c842a2ba2d9fbbd2f48dc
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-05 17:26:49 +0530

    extend and implement AbstractAnnotationConfigDispatcherServletInitializer

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/AppInitializer.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/AppInitializer.java
@@ -1,5 +1,23 @@
 package lk.ijse.gdse68.POS_System_REST_API;
 
-public class AppInitializer {
+import lk.ijse.gdse68.POS_System_REST_API.config.WebAppConfig;
+import lk.ijse.gdse68.POS_System_REST_API.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
