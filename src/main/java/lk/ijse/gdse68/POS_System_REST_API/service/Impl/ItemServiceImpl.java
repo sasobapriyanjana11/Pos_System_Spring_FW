3879f2f3857c740a06b2e12a27b68528863dd55c
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-09 21:27:12 +0530

    create ItemServiceImpl

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/service/Impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/service/Impl/ItemServiceImpl.java
@@ -1,4 +1,38 @@
 package lk.ijse.gdse68.POS_System_REST_API.service.Impl;
 
-public class ItemServiceImpl {
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse68.POS_System_REST_API.customObj.ItemResponse;
+import lk.ijse.gdse68.POS_System_REST_API.dto.impl.ItemDTO;
+import lk.ijse.gdse68.POS_System_REST_API.service.ItemService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@Transactional
+public class ItemServiceImpl implements ItemService {
+
+    @Override
+    public void saveItem(ItemDTO itemDTO) {
+
+    }
+
+    @Override
+    public List<ItemDTO> getAllItems() {
+        return null;
+    }
+
+    @Override
+    public ItemResponse getSelectedItem(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateItem(String id, ItemDTO itemDTO) {
+
+    }
+
+    @Override
+    public void deleteItem(String id) {
+
+    }
 }
