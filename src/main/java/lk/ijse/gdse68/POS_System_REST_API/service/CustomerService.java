8c3daac97328238c04ea4c7f9a228db487647d13
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-09 21:21:06 +0530

    create CustomerService

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/service/CustomerService.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/service/CustomerService.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse68.POS_System_REST_API.service;
 
+import lk.ijse.gdse68.POS_System_REST_API.customObj.CustomerResponse;
+import lk.ijse.gdse68.POS_System_REST_API.dto.impl.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDTO customerDTO);
+
+    List<CustomerDTO> getAllCustomers();
+
+    CustomerResponse getSelectedCustomer(String id);
+
+    void updateCustomer(String id, CustomerDTO customerDTO);
+
+    void deleteCustomer(String id);
 }
