f58479d4cd6e266e9b3c7650b7c6aad0c20c529c
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-09 21:25:19 +0530

    create Itemdao

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/dao/Itemdao.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/dao/Itemdao.java
@@ -1,5 +1,10 @@
 package lk.ijse.gdse68.POS_System_REST_API.dao;
 
-public interface Itemdao {
+import lk.ijse.gdse68.POS_System_REST_API.entity.ItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Itemdao extends JpaRepository<ItemEntity,String> {
 
 }
