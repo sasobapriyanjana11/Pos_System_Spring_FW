84c7b95b3cc2c5eba881773fec295d1cd8eb9162
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-09 21:24:03 +0530

    create HealthTestController and check the api works or not.

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/controller/HealthTestController.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/controller/HealthTestController.java
@@ -1,4 +1,14 @@
 package lk.ijse.gdse68.POS_System_REST_API.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/healthTest")
 public class HealthTestController {
+    @GetMapping
+    public String healthTesting(){
+        return "pos system api run successfully !";
+    }
 }
