2980ca407b7516921e3c2460493ccb39388e75fa
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-09 21:18:45 +0530

    create CustomerDTO.class and implementation

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/dto/impl/CustomerDTO.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/dto/impl/CustomerDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.gdse68.POS_System_REST_API.dto.impl;
 
-public class CustomerDTO {
+import lk.ijse.gdse68.POS_System_REST_API.customObj.CustomerResponse;
+import lk.ijse.gdse68.POS_System_REST_API.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO implements SuperDTO, CustomerResponse {
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
 }
