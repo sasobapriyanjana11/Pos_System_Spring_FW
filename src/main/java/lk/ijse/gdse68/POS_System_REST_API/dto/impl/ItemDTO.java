d5139e66e49c83604c6ad043d1f83fb25e3e098b
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-09 21:24:56 +0530

    create ItemDTO

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/dto/impl/ItemDTO.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/dto/impl/ItemDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse68.POS_System_REST_API.dto.impl;
 
-public class ItemDTO {
+import lk.ijse.gdse68.POS_System_REST_API.customObj.ItemResponse;
+import lk.ijse.gdse68.POS_System_REST_API.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDTO implements SuperDTO , ItemResponse {
+    private String code;
+    private String name;
+    private int qty;
+    private BigDecimal price;
 }
