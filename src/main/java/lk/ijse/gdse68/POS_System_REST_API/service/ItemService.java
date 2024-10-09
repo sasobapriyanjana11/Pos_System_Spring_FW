a35b039d8439cc60ee32374325a53cd7019fc505
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-09 21:26:46 +0530

    create ItemService

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/service/ItemService.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/service/ItemService.java
@@ -1,4 +1,20 @@
 package lk.ijse.gdse68.POS_System_REST_API.service;
 
+import lk.ijse.gdse68.POS_System_REST_API.customObj.CustomerResponse;
+import lk.ijse.gdse68.POS_System_REST_API.customObj.ItemResponse;
+import lk.ijse.gdse68.POS_System_REST_API.dto.impl.CustomerDTO;
+import lk.ijse.gdse68.POS_System_REST_API.dto.impl.ItemDTO;
+
+import java.util.List;
+
 public interface ItemService {
+    void saveItem(ItemDTO itemDTO);
+
+    List<ItemDTO> getAllItems();
+
+    ItemResponse getSelectedItem(String id);
+
+    void updateItem(String id, ItemDTO itemDTO);
+
+    void deleteItem(String id);
 }
