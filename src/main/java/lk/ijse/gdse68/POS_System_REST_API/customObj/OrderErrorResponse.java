312f50eb8047bb154f20a995844e4009121080bb
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-11 11:47:55 +0530

    create and implement exception and error handling in order

---
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/customObj/OrderErrorResponse.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse68.POS_System_REST_API.customObj;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OrderErrorResponse implements Serializable,OrderResponse {
+    private int errorCode;
+    private String errorMessage;
+}
---
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/exception/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse68.POS_System_REST_API.exception;
+
+public class OrderNotFoundException extends RuntimeException{
+    public OrderNotFoundException() {}
+    public OrderNotFoundException(String message) {}
+    public OrderNotFoundException(String message, Throwable cause) {}
+}
