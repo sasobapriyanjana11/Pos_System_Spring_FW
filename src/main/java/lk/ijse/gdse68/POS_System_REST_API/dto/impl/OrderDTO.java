73d8bef9df83bd8322b4a31337398a706bbef68e
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-11 11:53:35 +0530

    create order dto

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/dto/impl/OrderDTO.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/dto/impl/OrderDTO.java
@@ -1,4 +1,25 @@
 package lk.ijse.gdse68.POS_System_REST_API.dto.impl;
 
-public class OrderDTO {
+import lk.ijse.gdse68.POS_System_REST_API.customObj.OrderResponse;
+import lk.ijse.gdse68.POS_System_REST_API.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO implements SuperDTO, OrderResponse {
+    private String orderId;
+    private LocalDate orderDate;
+    private String custId;
+    private BigDecimal total;
+    private BigDecimal discount;
+    private BigDecimal subTotal;
+    private BigDecimal cash;
+    private BigDecimal balance;
+    private List<OrderDetailDTO> order_list;
 }
