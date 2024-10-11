35a0f8cd3187f9b948a5ce62ad1f55a3691feb0f
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-11 11:54:14 +0530

    create orderdao and exten jpaRepository

--- src/main/java/lk/ijse/gdse68/POS_System_REST_API/dao/Orderdao.java
+++ src/main/java/lk/ijse/gdse68/POS_System_REST_API/dao/Orderdao.java
@@ -1,5 +1,10 @@
 package lk.ijse.gdse68.POS_System_REST_API.dao;
 
-public interface Orderdao {
+import lk.ijse.gdse68.POS_System_REST_API.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Orderdao extends JpaRepository<OrderEntity,String> {
 
 }
